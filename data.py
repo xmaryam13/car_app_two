25db7ab903237553cbce1ac6b9e0117f2e98eea9
xmaryam13 <90093129+xmaryam13@users.noreply.github.com>
2021-09-18 12:54:50 +0100

    Add files via upload

---
+++ data.py
@@ -0,0 +1,34 @@
+# Code for 'data.py' file
+# Import necessary modules
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+# Define a function 'app()' which accepts 'car_df' as an input.
+def app(car_df):
+    st.header("View Data")
+    # Add an expander and display the dataset as a static table within the expander.
+    with st.beta_expander("View Dataset"):
+        st.table(car_df)
+
+    st.subheader("Columns Description:")
+    if st.checkbox("Show summary"):
+        st.table(car_df.describe())
+
+    beta_col1, beta_col2, beta_col3 = st.beta_columns(3)
+
+    # Add a checkbox in the first column. Display the column names of 'car_df' on the click of checkbox.
+    with beta_col1:
+        if st.checkbox("Show all column names"):
+            st.table(list(car_df.columns))
+
+    # Add a checkbox in the second column. Display the column data-types of 'car_df' on the click of checkbox.
+    with beta_col2:
+        if st.checkbox("View column data-type"):
+            st.table(car_df.dtypes)
+
+    # Add a checkbox in the third column followed by a selectbox which accepts the column name whose data needs to be displayed.
+    with beta_col3:
+        if st.checkbox("View column data"):
+            column_data = st.selectbox('Select column', tuple(car_df.columns))
+            st.write(car_df[column_data])
